acafdfda0889daf5a7db21b10d975f63c10ef697
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-28 22:51:13 +0200

    feat(exec): verify exec.LookPath

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -2,11 +2,29 @@ package exec_test
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/alexfalkowski/tausch/exec"
 	"github.com/stretchr/testify/require"
 )
+
+func TestPathCommandSuccess(t *testing.T) {
+	path := os.Getenv("PATH") + ":../"
+
+	t.Setenv("PATH", path)
+	t.Setenv("TAUSCH_CONFIG", "../test/configs/exec.yml")
+
+	b := &bytes.Buffer{}
+
+	cmd := exec.Command("go", "version")
+	cmd.Stdout = b
+	cmd.Stderr = b
+
+	require.NoError(t, cmd.Run())
+	require.NoError(t, cmd.Err)
+	require.NotEmpty(t, b.Bytes())
+}
 
 func TestCommandSuccess(t *testing.T) {
 	t.Setenv("TAUSCH_PATH", "../tausch")
