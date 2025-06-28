3039044ca6528f1e38565f366ef81a5b34bd1297
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-28 22:23:47 +0200

    test(exec): verify config and path

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -2,11 +2,46 @@ package exec_test
 
 import (
 	"bytes"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/alexfalkowski/tausch/exec"
 	"github.com/stretchr/testify/require"
 )
+
+func TestHomeCommandSuccess(t *testing.T) {
+	t.Setenv("TAUSCH_PATH", "../tausch")
+
+	home, err := os.UserHomeDir()
+	require.NoError(t, err)
+
+	dir := path.Join(home, ".config")
+
+	err = os.MkdirAll(dir, 0o600)
+	require.NoError(t, err)
+
+	d, err := os.ReadFile("../test/configs/exec.yml")
+	require.NoError(t, err)
+
+	config := path.Join(dir, "tausch.yml")
+
+	err = os.WriteFile(config, d, 0o600)
+	require.NoError(t, err)
+
+	b := &bytes.Buffer{}
+
+	cmd := exec.Command("go", "version")
+	cmd.Stdout = b
+	cmd.Stderr = b
+
+	require.NoError(t, cmd.Run())
+	require.NoError(t, cmd.Err)
+	require.NotEmpty(t, b.Bytes())
+
+	err = os.Remove(config)
+	require.NoError(t, err)
+}
 
 func TestCommandSuccess(t *testing.T) {
 	t.Setenv("TAUSCH_PATH", "../tausch")
