d5fde9c0a6ca8a2068a90a3c82dcb77a0663c853
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-28 05:37:42 +0200

    feat(cmd): pass stdout/stderr to cmd

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,15 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/alexfalkowski/tausch/internal/config"
 	"github.com/alexfalkowski/tausch/internal/flag"
 	"github.com/alexfalkowski/tausch/internal/io"
 )
 
-// Run will get the command as write to the correct writer.
-func Run() (int, error) {
+// Run will get the command and write to the specified writer.
+func Run(stdout, stderr io.Writer) (int, error) {
 	file := flag.Config()
 	name := flag.Name()
 
@@ -24,12 +23,12 @@ func Run() (int, error) {
 		return 0, fmt.Errorf("find %s: %w", name, err)
 	}
 
-	ok, err := io.Write(command.Stdout, os.Stdout)
+	ok, err := io.Write(stdout, command.Stdout)
 	if err != nil || ok {
 		return 0, err
 	}
 
-	_, err = io.Write(command.Stderr, os.Stderr)
+	_, err = io.Write(stderr, command.Stderr)
 
 	return 1, err
 }
