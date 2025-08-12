9c5105da00c94306ea1e8c7ea27e0ac3b1101087
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-08-12 21:22:45 +0200

    feat(flag): move to values (#41)

---
+++ internal/flag/values.go
@@ -0,0 +1,47 @@
+package flag
+
+import (
+	"cmp"
+	"flag"
+	"os"
+	"path"
+	"strings"
+)
+
+// NewValues parses the args and extracts the config file and command name.
+func NewValues(args []string) (*Values, error) {
+	set := flag.NewFlagSet("tausch", flag.ContinueOnError)
+
+	file := set.String("config", "", "the config file path")
+	if err := set.Parse(args); err != nil {
+		return nil, err
+	}
+
+	return &Values{file: *file, args: set.Args()}, nil
+}
+
+// Values represents the config and name of the command.
+type Values struct {
+	file string
+	args []string
+}
+
+// Config file.
+func (f *Values) Config() (string, error) {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	config := cmp.Or(
+		f.file,
+		os.Getenv("TAUSCH_CONFIG"),
+		path.Join(dir, "tausch", "config.yml"),
+	)
+	return config, nil
+}
+
+// Name of the command.
+func (f *Values) Name() string {
+	return strings.TrimSpace(strings.Join(f.args, " "))
+}
